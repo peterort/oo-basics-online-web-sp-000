9fb73d71de6c5b3a01c529505ce6778dd1afd614
Peter Ort <peter@peterort.com>
2020-05-25 15:10:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,15 +2,15 @@ class Book
   def initialize(title)
     @title = title
   end
-  
+
   def title
     @title
   end
-  
+
   def author=(author)
     @author = author
   end
-  
+
   def author
     @author
   end
